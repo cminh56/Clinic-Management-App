94d63963e8cd23d419446db97ae2e1fbdefa5ebf
15-Vũ Đình Khiêm <khiemvdhe170306@fpt.edu.vn>
2024-06-14 18:12:00 +0700

    project base

---
+++ ProjectHospital/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjectHospital
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
