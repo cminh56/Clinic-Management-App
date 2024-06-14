fd86e18716f218c5c96de4c7589dd01cf7337060
Nguyen Quang Vinh <VinhNQ45.fpt.com>
2024-06-14 23:07:20 +0700

    do EF core

--- ProjectClinicManagement/ProjectClinicManagement/App.xaml.cs
+++ ProjectClinicManagement/ProjectClinicManagement/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectClinicManagement.Data;
+using System;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +13,23 @@ namespace ProjectClinicManagement
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddTransient<MainWindow>();
+        }
     }
 
 }
---
+++ ProjectClinicManagement/ProjectClinicManagement/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProjectClinicManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClinicManagement.Data
+{
+    public class DataContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            string connectionString = config.GetConnectionString("Dbcontext");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
