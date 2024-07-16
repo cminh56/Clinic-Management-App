b871dd20e18fe5ab4389e30b4170c5474f61ac9d
Nguyen Quang Vinh <VinhNQ45.fpt.com>
2024-07-17 00:06:35 +0700

    do logout

--- ProjectClinicManagement/ProjectClinicManagement/ViewModel/AdminViewModel/AdminNavigationVM.cs
+++ ProjectClinicManagement/ProjectClinicManagement/ViewModel/AdminViewModel/AdminNavigationVM.cs
@@ -1,11 +1,14 @@
 ﻿using ProjectClinicManagement.Command;
 using ProjectClinicManagement.ViewModel.Common;
+using ProjectClinicManagement.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ProjectClinicManagement.ViewModel.AdminViewModel
 {
@@ -24,11 +27,13 @@ namespace ProjectClinicManagement.ViewModel.AdminViewModel
 
         public RelayCommand NavigateToPage1Command { get; }
         public RelayCommand NavigateToPage2Command { get; }
+        public ICommand LogoutCommand { get; }
 
         public AdminNavigationVM()
         {
             NavigateToPage1Command = new RelayCommand(_ => NavigateToPage1());
             NavigateToPage2Command = new RelayCommand(_ => NavigateToPage2());
+            LogoutCommand = new RelayCommand(Logout);
             CurrentPage = new Views.Admin.ViewUsers();
         }
 
@@ -40,6 +45,25 @@ namespace ProjectClinicManagement.ViewModel.AdminViewModel
         private void NavigateToPage2()
         {
             CurrentPage = new Views.Page2();
+        }
+        private void Logout(object? parameter)
+        {
+            // Clear user information
+            Application.Current.Properties["UserName"] = null;
+            Application.Current.Properties["UserRole"] = null;
+
+            // Reopen login window
+            Login loginWindow = new Login();
+            loginWindow.Show();
+
+            // Close current window
+            if (Application.Current.MainWindow != null)
+            {
+                Application.Current.MainWindow.Close();
+            }
+
+            // Set current window to login window
+            Application.Current.MainWindow = loginWindow;
         }
     }
 }
