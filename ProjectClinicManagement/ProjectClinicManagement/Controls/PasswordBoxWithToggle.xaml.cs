c4149ed06eed3a20b9ba20f459116d94ba62aa54
LAPTOP-EKG8C6KJ\maivu <maivubao08012002@gmail.com>
2024-07-20 10:08:58 +0700

    bukhu

---
+++ ProjectClinicManagement/ProjectClinicManagement/Controls/PasswordBoxWithToggle.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ProjectClinicManagement.Controls
+{
+    public partial class PasswordBoxWithToggle : UserControl
+    {
+        public PasswordBoxWithToggle()
+        {
+            InitializeComponent();
+            TextBox.TextChanged += (s, e) => PasswordBox.Password = TextBox.Text;
+            PasswordBox.PasswordChanged += (s, e) => TextBox.Text = PasswordBox.Password;
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Visibility = Visibility.Collapsed;
+            TextBox.Visibility = Visibility.Visible;
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Visibility = Visibility.Visible;
+            TextBox.Visibility = Visibility.Collapsed;
+        }
+
+        public string Password
+        {
+            get => PasswordBox.Password;
+            set
+            {
+                PasswordBox.Password = value;
+                TextBox.Text = value;
+            }
+        }
+    }
+}
