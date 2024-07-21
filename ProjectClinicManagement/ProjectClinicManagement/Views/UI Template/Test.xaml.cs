144589365f73619e5c56f0405d6622ab918c0454
LAPTOP-EKG8C6KJ\maivu <maivubao08012002@gmail.com>
2024-07-21 20:23:22 +0700

    .

---
+++ ProjectClinicManagement/ProjectClinicManagement/Views/UI Template/Test.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectClinicManagement.Views.UI_Template
+{
+    /// <summary>
+    /// Interaction logic for Test.xaml
+    /// </summary>
+    public partial class Test : Window
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
